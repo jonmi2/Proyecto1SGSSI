fa0336b687afc1e5393a4da9cdbe8a37f70441ef
Jon Miraz <jonmiamor@gmail.com>
2024-11-22 16:56:42 +0100

    he comentado Las cuentas por defecto y sus contraseñas siguen activas

--- app/db.php
+++ app/db.php
@@ -1,21 +1,13 @@
 <?php
-$hostname = "db";
-$username = "admin";
-$password = "test";
-$db = "database";
-
-$conn = mysqli_connect($hostname, $username, $password, $db);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
-
-<?php
 // Obtén las credenciales desde las variables de entorno
-$hostname = "db";                   // El nombre del servicio de la base de datos en docker-compose
-$username = getenv('MYSQL_USER');    // MYSQL_USER desde el archivo .env
-$password = getenv('MYSQL_PASSWORD'); // MYSQL_PASSWORD desde el archivo .env
-$db = getenv('MYSQL_DATABASE');      // MYSQL_DATABASE desde el archivo .env
+$hostname = "db";  // El nombre del servicio de la base de datos en docker-compose
+$username = "admin";   
+$password = "test"; 
+$db = "database";      
+
+//$username = getenv('MYSQL_USER');    // MYSQL_USER desde el archivo .env
+//$password = getenv('MYSQL_PASSWORD'); // MYSQL_PASSWORD desde el archivo .env
+//$db = getenv('MYSQL_DATABASE');      // MYSQL_DATABASE desde el archivo .env
 
 // Conecta a la base de datos
 $conn = mysqli_connect($hostname, $username, $password, $db);
